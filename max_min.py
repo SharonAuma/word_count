43155785fafc964b2f629c891a594f8889edf242
SharonAuma <sharonusenge90@gmail.com>
2016-12-02 16:12:17 +0300

    Create max_min.py

---
+++ max_min.py
@@ -0,0 +1,10 @@
+list1=[1,2,3,4]
+print (["Min value element : ", min(list1),"Max value element : ", max(list1)])
+list2=[6,4]
+print (["Min value element : ", min(list2), "Max value element : ", max(list2)])
+list3=[4,66,6,4,7,78,8,68,2]
+print (["Min value element : ", min(list3),"Max value element : ", max(list3)])
+list4=[1,2,3,4]
+print (["Min value element : ", min(list4), "Max value element : ", max(list4)])
+list5=[4,4,4,4]
+print (["4,4,4,4"])

717d8a2dd06328cfa563d265a456a51dc7b6924b
SharonAuma <sharonusenge90@gmail.com>
2016-12-02 18:13:42 +0300

    Update api.py

--- api.py
+++ api.py
@@ -1,3 +1,40 @@
 import requests
 kittens=requests.get("http://placekitten.com/")
 print kittens.text[559:1000]
+
+
+import sys
+import pycurl
+
+wr_buf = ''
+
+def write_data( buf ):
+	global wr_buf
+	wr_buf += buf
+
+def main():
+	c = pycurl.Curl()
+	c.setopt( pycurl.URL, 'http://www.googlemaps.com' )
+	c.setopt( pycurl.googlemaps, write_data )
+
+	c.perform()
+
+	c.close()
+
+main()
+sys.stdout.write(wr_buf)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

3d6a276753bc8c5b266780ef3809b3c9fc4aa861
SharonAuma <sharonusenge90@gmail.com>
2016-12-02 16:09:10 +0300

    Create wordcount.py

---
+++ wordcount.py
@@ -0,0 +1,35 @@
+def words(word):
+ s_word = word.lower().split()
+ s_dict = {}
+ for item in s_word:
+   s_dict[item] = s_word.count(item)
+ print(s_dict)
+
+words("word")
+
+def words(word):
+ s_word = word.lower().split()
+ s_dict = {}
+ for item in s_word:
+   s_dict[item] = s_word.count(item)
+ print(s_dict)
+
+words("one of each")
+
+def words(word):
+ s_word = word.lower().split()
+ s_dict = {}    
+ for item in s_word:
+   s_dict[item] = s_word.count(item)
+ print(s_dict)
+
+words("one fish two fish red fish blue fish")
+
+def words(word):
+ s_word = word.lower().split()
+ s_dict = {}
+ for item in s_word:
+   s_dict[item] = s_word.count(item)
+ print(s_dict)
+
+words("car : carpet as java : javascript!!&@$%^&")
